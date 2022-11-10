0ac6326c330e5a79b44622f45a1b38ffd82ce207
TheRedSpy43 <61850423+TheRedSpy43@users.noreply.github.com>
2022-11-10 10:06:13 -0600

    Hope this works

---
+++ PythonTest/PythonTest/PythonTest.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+features = np.array([[-100.1, 3240.1],
+                     [-200.2, -234.1],
+                     [5000.5, 150.1],
+                     [6000.0, -125.1],
+                     [9000.0, -673.1]])
+# this is a test by John
+#this is a test to see if anything works
