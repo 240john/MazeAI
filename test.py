854e549608d44848b24a8e18a8f1127c7445b807
Teddy Openhowski <teddyopenhowski@gmail.com>
2022-11-10 11:31:07 -0600

    changing test

--- test.py
+++ test.py
@@ -6,4 +6,7 @@ features = np.array([[-100.1, 3240.1],
                      [6000.0, -125.1],
                      [9000.0, -673.1]])
 print(features)
+
+print("printing features again")
+print(features)
 # this is a test by John
