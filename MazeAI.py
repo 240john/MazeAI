98b98e08e16aacc8865aeafe7d31542e03f71d9b
John Eng <John.D.E@hotmail.com>
2022-11-27 18:08:31 -0600

    added maze generation functionality

--- MazeAI.py
+++ MazeAI.py
@@ -3,62 +3,124 @@ import time
 import pygame
 import pygame.gfxdraw as gfx
 import random
+import maze
 
-gen = 0
-popSize = 2000
-stepCount = 60 # initial amount of steps allowed - increases over time
+walls = [] # List to hold the walls
 
-# size of the maze
-mazeWidth = 800
-mazeHeight = 800
+class Wall(object):
+    def __init__(self, pos):
+        walls.append(self)
+        self.rect = pygame.Rect(pos[0], pos[1], 16, 16)
 
-background = pygame.display.set_mode((mazeWidth,mazeHeight))
+def Maze_generation():
+    gen = 0
+    popSize = 200
+    stepCount = 60 # initial amount of steps allowed - increases over time
 
-rectangle = ((10,10), (4,4))
-numb1 = 10
-numb2 = 10
+    # two arrays to hold all values of current rectangles, currently values all are 0
+    RectanglesX = np.empty(popSize, dtype=int) 
+    RectanglesX = [0 for i in range(popSize)] 
+    RectanglesY = np.empty(popSize, dtype=int)
+    RectanglesY = [0 for i in range(popSize)]
 
-background.fill((255, 255, 255))
+    #array for rectangle colors
+    RectanglesColor = np.empty(popSize, dtype=int)
+    RectanglesColor = [0 for i in range(popSize)]
 
-pygame.init()
- 
-# displaying a window of height
-# 500 and width 400
-pygame.display.set_mode((mazeWidth, mazeHeight))
- 
-# Setting name for window
-pygame.display.set_caption('Testing')
- 
-# creating a bool value which checks
-# if game is running
-running = True
- 
-# Game loop
-# keep game running till running is true
-while running:
-   
-    # Check for event if user has pushed
-    # any event in queue
-    for event in pygame.event.get():
-       
-        # if event is of type quit then set
-        # running bool to false
-        if event.type == pygame.QUIT:
-            running = False
-     
-    # Update our window
-    # example of random movement follows this comment
-    list = [-1,0,1]
-    RandNum1 = (random.choice(list))
-    RandNum2 = (random.choice(list))
-    while numb1 + RandNum1 < 0 or numb1 + RandNum1 > 799:
-        RandNum1 = (random.choice(list))
-    while numb2 + RandNum2 < 0 or numb2 + RandNum2 > 799:
-        RandNum2 = (random.choice(list))
-    numb1 = numb1 + RandNum1
-    numb2 = numb2 + RandNum2
+    #assigns rectangle colors
+    for populationCounter in range(popSize):
+        RandColorVal = (random.randrange(1, 16777217))
+        RectanglesColor[populationCounter] = RandColorVal
+
+    # size of the maze
+    mazeWidth = 800
+    mazeHeight = 800
+
+    background = pygame.display.set_mode((mazeWidth,mazeHeight))
+
+    # Holds the level layout in a list of strings.
+    level = maze.create_maze()
+
+    # Parse the level string above. W = wall, E = exit
+    x = y = 0
+    for row in level:
+        for col in row:
+            if col == "W":
+                Wall((x, y))
+            if col == "X":
+                end_rect = pygame.Rect(x, y, 16, 16)
+            x += 16
+        y += 16
+        x = 0
+
+    rectangle = ((10,10), (4,4))
+    numb1 = 10
+    numb2 = 10
+
     background.fill((255, 255, 255))
-    rectangle = ((numb1,numb2), (4,4))
-    pygame.draw.rect(background,(50,100,250,255),rectangle,2)
-    pygame.display.flip()
-    time.sleep(.01)
+
+    pygame.init()
+    
+    # displaying a window of height
+    # 500 and width 400
+    pygame.display.set_mode((mazeWidth, mazeHeight))
+    
+    # Setting name for window
+    pygame.display.set_caption('Maze AI')
+    
+    # creating a bool value which checks
+    # if game is running
+    running = True
+    
+    # Game loop
+    # keep game running till running is true
+    while running:
+    
+        # Check for event if user has pushed
+        # any event in queue
+        for event in pygame.event.get():
+        
+            # if event is of type quit then set
+            # running bool to false
+            if event.type == pygame.QUIT:
+                running = False
+
+    #--Random movement for ALL rectangles, values are randomed and added to arrays on lines 14-16
+        for populationCounter in range(popSize):
+            list = [-1,0,1]
+            RandNum1 = (random.choice(list))
+            RandNum2 = (random.choice(list))
+            while RectanglesX[populationCounter] + RandNum1 < 0 or RectanglesX[populationCounter] + RandNum1 > 799:
+                RandNum1 = (random.choice(list))
+            while RectanglesY[populationCounter] + RandNum2 < 0 or RectanglesY[populationCounter] + RandNum2 > 799:
+                RandNum2 = (random.choice(list))
+            RectanglesX[populationCounter] = RectanglesX[populationCounter] + RandNum1
+            RectanglesY[populationCounter] = RectanglesY[populationCounter] + RandNum2
+            
+
+    #---Previous random code for 1 Rectangle-----------------------
+        #    list = [-1,0,1]
+        #    RandNum1 = (random.choice(list))
+        #    RandNum2 = (random.choice(list))
+        #    while numb1 + RandNum1 < 0 or numb1 + RandNum1 > 799:
+        #        RandNum1 = (random.choice(list))
+        #    while numb2 + RandNum2 < 0 or numb2 + RandNum2 > 799:
+        #        RandNum2 = (random.choice(list))
+        #    numb1 = numb1 + RandNum1
+        #    numb2 = numb2 + RandNum2
+    #---------------------------------------------------------------
+
+    #set background to white, then fill in rectangles following array
+        background.fill((255, 255, 255))
+        for populationCounter in range(popSize):
+            rectangle = ((RectanglesX[populationCounter],RectanglesY[populationCounter]), (4,4))
+            pygame.draw.rect(background,(RectanglesColor[populationCounter]),rectangle,2)
+            #(50,100,250,255)
+        for wall in walls:
+            pygame.draw.rect(background, (0, 0, 0), wall.rect)
+
+
+        pygame.display.flip()
+        time.sleep(.001) #edit to adjust speed
+
+#Maze_generation()
---
+++ maze.py
@@ -0,0 +1,44 @@
+import numpy as np
+import random
+from random import randrange
+
+def create_maze():
+    level = np.full((22, 22), 'W')
+        
+    x = randrange(22)
+    y = 0
+    endX = randrange(22)
+    endY = 21
+    level[x][y] = 'X'
+    level[endX][endY] = 'X'
+    foundX = False
+    list1 = [-1,1,1,1]
+    list2 = [-1,-1,-1,1]
+    
+    while not foundX:
+        # select cell next to currentCell and ensure it's inbounds
+        while True:
+            if randrange(2) == 0:
+                if x < endX:
+                    num = random.choice(list1)
+                else:
+                    num = random.choice(list2)
+                if x + num > 0 and x + num < 21 and y > 0:
+                    x = x + num
+                    break
+            else:  
+                if y < endY:
+                    num = random.choice(list1)
+                else:
+                    num = random.choice(list2)
+                if y + num > 0 and y + num < 21:
+                    y = y + num
+                    break   
+
+        level[x][y] = ' '
+
+        # check to see if we found 'X' in the last row
+        if y == endY-1 and x == endX:
+            foundX = True
+
+    return level
